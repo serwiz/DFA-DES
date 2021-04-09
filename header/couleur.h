a18c608c1bdecfbdbfbb61b79cd82cfbcecb2e3c
Wissam Serhan <wissam.serhan@outlook.fr>
2021-04-09 20:23:15 +0200

    Refractoring de la fonction info

--- header/couleur.h
+++ header/couleur.h
@@ -4,6 +4,7 @@
 
 #define couleur(param) printf("\033[%sm", param)
 
+
 #define BLEU "34"
 #define ROUGE "31"
 #define VERT "32"
@@ -12,5 +13,10 @@
 #define CYAN "36"
 #define MAGENTA "35"
 #define BLANC "37"
+
+/**
+ * Affiche le status [INFO] dans le terminal
+ **/
+void info();
 
 #endif
--- src/main.c
+++ src/main.c
@@ -28,29 +28,17 @@ int main(int argc, char** argv) {
         printf("Error: Initialisation failed.");
         return 2;
     }
-    printf("[");
-    couleur(BLEU);
-    printf("INFO");
-    couleur(O);
-    printf("]");
+    info();
     printf("        Researching secret key K...\n\n");
     
     uint64_t K16 = generateK16(encrypted_message, encrypted_wrong);
-    printf("[");
-    couleur(BLEU);
-    printf("INFO");
-    couleur(O);
-    printf("]");
+    info();
     printf("        Key K16 found:\n");
     couleur("1");
     printf("                    %lx \n\n", K16);
     couleur(O);
 
-    printf("[");
-    couleur(BLEU);
-    printf("INFO");
-    couleur(O);
-    printf("]");
+    info();
     printf("        Reverse key schreduling on K16...");
     K16 = reverse_keyschred(K16);
     printf("  Done:\n");
@@ -59,11 +47,7 @@ int main(int argc, char** argv) {
     couleur(O);
 
     uint64_t K;
-    printf("[");
-    couleur(BLEU);
-    printf("INFO");
-    couleur(O);
-    printf("]");
+    info();
     printf("        Exhaustive attack on missing bits...");
     K = exhaustive_attack(clear_message, encrypted_message, K16);
     printf("  Done:\n");
@@ -71,11 +55,7 @@ int main(int argc, char** argv) {
     printf("                    %lx \n\n", K);
     couleur(O);
 
-    printf("[");
-    couleur(BLEU);
-    printf("INFO");
-    couleur(O);
-    printf("]");
+    info();
     printf("        Adding parity bits...");
     K = add_parity_bits(K);
     printf("  Done:\n");
@@ -83,24 +63,22 @@ int main(int argc, char** argv) {
     printf("                    %lx \n\n", K);
     couleur(O);
 
-    printf("[");
-    couleur(BLEU);
-    printf("INFO");
-    couleur(O);
-    printf("]");
+    info();
     printf("        Secret key found :\n\n");
     couleur(JAUNE);
     couleur("1");
     printf("                    %lx\n\n", K);
     couleur(O);
 
-    printf("[");
-    couleur(BLEU);
-    printf("INFO");
-    couleur(O);
-    printf("]");
+    info();
     printf("        Checking DES output with secret key:\n");
-    if(DES(clear_message, K) == encrypted_message) {
+    
+    uint64_t encrypted_found = DES(clear_message, K);
+    info();
+    printf("        Encrypted given : %lx\n", encrypted_message);
+    info();
+    printf("        Encrypted found : %lx\n", encrypted_found);
+    if(encrypted_found == encrypted_message) {
         couleur(VERT);
         couleur("1");
         couleur("5");
@@ -121,4 +99,25 @@ int main(int argc, char** argv) {
     couleur(O);
     free(encrypted_wrong);
     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void info() {
+    printf("[");
+    couleur(BLEU);
+    printf("INFO");
+    couleur(O);
+    printf("]");
 }
